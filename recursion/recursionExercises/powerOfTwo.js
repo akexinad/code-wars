a47d4083950bd88b5301af8245ac81161cd850dd
danny <danixeka@gmail.com>
2019-12-26 18:19:10 +0100

    recursion - power of two complete

--- recursion/recursionExercises/powerOfTwo.js
+++ recursion/recursionExercises/powerOfTwo.js
@@ -4,21 +4,27 @@
 // powerOfTwo(10); // false
 // var powerOfTwo = function(n) {};
 
-function powerOfTwo(num, result = 1) {
+function powerOfTwo(num, check = 2) {
+    if (num === 1) return true;
 
-    console.log(num);
-    console.log(result);
-
-    if (num === result) {
-        return true;
+    while (check < num) {
+        check **= 2;
+        if (check === num) return true;
+        return powerOfTwo(num, check);
     }
 
-    if (result > num) return false;
-
-    result = num ** 2;
-
-    return powerOfTwo(num, result);
+    return false;
 }
 
-const foo = powerOfTwo(4);
+// const foo = powerOfTwo(1);
+// const foo = powerOfTwo(2);
+// const foo = powerOfTwo(4);
+// const foo = powerOfTwo(16);
+// const foo = powerOfTwo(10);
+// const foo = powerOfTwo(34353);
+// const foo = powerOfTwo(1234);
+// const foo = powerOfTwo(84673);
+// const foo = powerOfTwo(256);
+// const foo = powerOfTwo(65536);
+const foo = powerOfTwo(4294967296);
 console.log(foo);
