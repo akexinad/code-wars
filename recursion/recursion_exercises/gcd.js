be7eb57ae8d5184d36b2deba1ab3aa1066b1566c
danny <danixeka@gmail.com>
2019-12-28 12:29:14 +0100

    file refactor

---
+++ recursion/recursion_exercises/gcd.js
@@ -0,0 +1,14 @@
+// 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
+// integers is the greatest integer that divides both x and y with no remainder.
+// gcd(4,36); // 4
+// http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
+// https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+// var gcd = function(x, y) {
+// };
+
+function gcd(x, y) {
+    
+}
+
+const foo = gcd(4, 36);
+console.log(foo);
